6c10a4cb42d36e8c5e4bf7a8920ed96fc92954d8
Artur Nikkonen <artur.nikkonen@gmail.com>
2020-05-27 00:22:15 +0300

    Переделал NavigationTests, чтобы в нем проверялось меню

--- src/test/java/ru/geekbrains/main/site/at/GeekBrainsBaseTests.java
+++ src/test/java/ru/geekbrains/main/site/at/GeekBrainsBaseTests.java
@@ -3,12 +3,10 @@ package ru.geekbrains.main.site.at;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
